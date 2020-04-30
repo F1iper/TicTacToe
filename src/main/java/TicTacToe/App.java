11d356b6c5b06b21e4cc7d553010502f3a41ea06
Filip <ftimofiejew@gmail.com>
2020-04-30 22:42:11 +0200

    JDK 1.8 updated

---
+++ src/main/java/TicTacToe/App.java
@@ -0,0 +1,9 @@
+package TicTacToe;
+
+public class App {
+    
+    public static void main (String[] args) throws Exception {
+        Runner runner = new Runner();
+        runner.run();
+    }
+}
