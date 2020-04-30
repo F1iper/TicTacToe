923588fe2271b64fdb58c86314ec44d535c712f1
Filip <ftimofiejew@gmail.com>
2020-04-30 22:44:07 +0200

    JDK 1.8 added

--- src/main/java/TicTacToe/App.java
+++ src/main/java/TicTacToe/App.java
@@ -1,9 +1,17 @@
 package TicTacToe;
 
-public class App {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
     
     public static void main (String[] args) throws Exception {
         Runner runner = new Runner();
         runner.run();
+    }
+    
+    @Override
+    public void start (Stage primaryStage) throws Exception {
+    
     }
 }
